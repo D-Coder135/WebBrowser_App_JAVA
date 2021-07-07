55c7219c2c2cf3b3e8c76b1c49810c9c410ae8be
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-07 20:20:19 +0530

    initialize the object editText

--- app/src/main/java/com/example/webbrowser_app_java/MainActivity.java
+++ app/src/main/java/com/example/webbrowser_app_java/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
+        editText = findViewById(R.id.editText);
     }
 }
