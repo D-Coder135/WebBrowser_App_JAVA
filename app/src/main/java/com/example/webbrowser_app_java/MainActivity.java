fd978fad22409ee9e429865e3e21774d4aa73621
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-07 20:17:14 +0530

    define the object for the WebView class

--- app/src/main/java/com/example/webbrowser_app_java/MainActivity.java
+++ app/src/main/java/com/example/webbrowser_app_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.webbrowser_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
